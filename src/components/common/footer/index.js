f41b31291d962aaab472c34da748370f563469a7
amrendrait43@gmail.com <Nh2conh2@aa432>
2022-06-11 19:06:42 +0530

    Zomato-clone

---
+++ src/components/common/footer/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="absolute-center max-width">Made 🔥 by Amrendra Chaudhary</div>
+  );
+};
+
+export default Footer;
